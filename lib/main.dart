0153e906849cad635fc41d3d9947115cca2081ce
Joseph Tay <55121849+Joseph-Tay@users.noreply.github.com>
2021-04-25 16:56:34 -0700

    Updated Title and font sizes to fit boxes
    Changed title, changed font size for box buttons (Number + email + URL)

--- lib/main.dart
+++ lib/main.dart
@@ -5,121 +5,120 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  @override 
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius:50.0,
-                backgroundImage: AssetImage('images/jtay.jpg'),
-              ),
-              Text(
-                'Joseph Tay',
-                style: TextStyle(
-                  fontFamily:'RobotoMono',
-                  fontSize: 30.0,
-                  color: Colors.white,
+        home: Scaffold(
+            backgroundColor: Colors.blueGrey,
+            body: SafeArea(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/jtay.jpg'),
+                ),
+                Text(
+                  'Joseph Tay',
+                  style: TextStyle(
+                    fontFamily: 'RobotoMono',
+                    fontSize: 30.0,
+                    color: Colors.white,
                   ),
                 ),
-              Text(
-                'Engineer | Aspiring Developer',
-                style: TextStyle(
-                  fontFamily:'RobotoMono',
-                  fontSize: 20.0,
-                  color: Colors.white,
+                Text(
+                  'Engineer | Program Manager',
+                  style: TextStyle(
+                    fontFamily: 'RobotoMono',
+                    fontSize: 20.0,
+                    color: Colors.white,
                   ),
-              ),
-              SizedBox(
-                height:30.0,
-                width:200.0,
-                child: Divider(
-                  color: Colors.blueGrey.shade900,
                 ),
-              ),
-              GestureDetector(
-                onTap:(){
-                  _launchCALL();
-                },
-                child: Card(
-                  margin: EdgeInsets.symmetric(vertical:10.0, horizontal:50.0),
+                SizedBox(
+                  height: 30.0,
+                  width: 200.0,
+                  child: Divider(
+                    color: Colors.blueGrey.shade900,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _launchCALL();
+                  },
+                  child: Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
                     child: ListTile(
                       leading: Icon(
-                          Icons.phone,
-                          color: Colors.blueGrey.shade800,
-                        ),
+                        Icons.phone,
+                        color: Colors.blueGrey.shade800,
+                      ),
                       title: Text(
-                          '+1 (949) 407-9728 ',
-                          style: TextStyle(
-                            color: Colors.blueGrey.shade800,
-                            fontFamily:'BreeSerif',
-                            fontSize: 20.0,
-                          ),
+                        '+1 (949) 407-9728 ',
+                        style: TextStyle(
+                          color: Colors.blueGrey.shade800,
+                          fontFamily: 'BreeSerif',
+                          fontSize: 18.0,
                         ),
+                      ),
                     ),
+                  ),
                 ),
-              ),
-              GestureDetector(
-                 onTap:(){
-                  _launchMAIL();
-                },
-                child: Card(
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+                GestureDetector(
+                  onTap: () {
+                    _launchMAIL();
+                  },
+                  child: Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
                     child: ListTile(
                       leading: Icon(
-                          Icons.email,
-                          color: Colors.blueGrey.shade800,
-                        ),
+                        Icons.email,
+                        color: Colors.blueGrey.shade800,
+                      ),
                       title: Text(
-                          'joseph.tqy@gmail.com ',
-                          style: TextStyle(
-                            color: Colors.blueGrey.shade800,
-                            fontFamily:'BreeSerif',
-                            fontSize: 20.0,
-                          ),
+                        'joseph.tqy@gmail.com ',
+                        style: TextStyle(
+                          color: Colors.blueGrey.shade800,
+                          fontFamily: 'BreeSerif',
+                          fontSize: 18.0,
                         ),
+                      ),
                     ),
+                  ),
                 ),
-              ),
-              GestureDetector(
-                onTap:(){
-                  _launchURL();
-                },
-                child: Card(
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+                GestureDetector(
+                  onTap: () {
+                    _launchURL();
+                  },
+                  child: Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
                     child: ListTile(
                       leading: Icon(
-                          Icons.language,
-                          color: Colors.blueGrey.shade800,
-                        ),
+                        Icons.language,
+                        color: Colors.blueGrey.shade800,
+                      ),
                       title: Text(
-                          'joseph-tay.github.io ',
-                          style: TextStyle(
-                            color: Colors.blueGrey.shade800,
-                            fontFamily:'BreeSerif',
-                            fontSize: 20.0,
-                          ),
+                        'joseph-tay.github.io ',
+                        style: TextStyle(
+                          color: Colors.blueGrey.shade800,
+                          fontFamily: 'BreeSerif',
+                          fontSize: 18.0,
                         ),
+                      ),
                     ),
+                  ),
                 ),
-              ), 
-            ],
-          )
-        )
-      )
-    );
+              ],
+            ))));
   }
 }
 
-
 _launchURL() async {
-    const url = 'https://joseph-tay.github.io';
-    if (await canLaunch(url)) {
+  const url = 'https://joseph-tay.github.io';
+  if (await canLaunch(url)) {
     await launch(url);
   } else {
     throw 'Could not launch $url';
@@ -127,8 +126,8 @@ _launchURL() async {
 }
 
 _launchMAIL() async {
-    const url = 'mailto:joseph.tqy@gmail.com';
-    if (await canLaunch(url)) {
+  const url = 'mailto:joseph.tqy@gmail.com';
+  if (await canLaunch(url)) {
     await launch(url);
   } else {
     throw 'Could not launch $url';
@@ -136,8 +135,8 @@ _launchMAIL() async {
 }
 
 _launchCALL() async {
-    const url = 'tel:+1 949 407 9728';
-    if (await canLaunch(url)) {
+  const url = 'tel:+1 949 407 9728';
+  if (await canLaunch(url)) {
     await launch(url);
   } else {
     throw 'Could not launch $url';
